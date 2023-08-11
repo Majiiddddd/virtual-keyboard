5a4ca7a0407cde5110b38b5a163591d3a51f8a3f
Majid Abolfathi <107215137+Majiiddddd@users.noreply.github.com>
2023-08-11 10:03:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('keydown' , (event)=>{
+    document.getElementById(event.key).classList.add('pressed')
+})
+
+
+window.addEventListener('keyup' , (event)=>{
+    document.getElementById(event.key).classList.remove('pressed')
+})
+
+
+
+
+
